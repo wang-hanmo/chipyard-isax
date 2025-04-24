596c2a86aef4f539337575dfc0dc8cef901492c7
Wang Hanmo <wanghanmo@stu.pku.edu.cn>
2025-04-24 10:25:52 +0000

    delete trash files

--- verilator_test/obj_dir/test.d
+++
@@ -1,9 +0,0 @@
-test.o: ../test.cpp Vtest.h \
- /mnt/disk/newhome_20241022/wanghanmo/chipyard-isax/.conda-env/share/verilator/include/verilated.h \
- /mnt/disk/newhome_20241022/wanghanmo/chipyard-isax/.conda-env/share/verilator/include/verilatedos.h \
- /mnt/disk/newhome_20241022/wanghanmo/chipyard-isax/.conda-env/share/verilator/include/verilated_config.h \
- /mnt/disk/newhome_20241022/wanghanmo/chipyard-isax/.conda-env/share/verilator/include/verilated_types.h \
- /mnt/disk/newhome_20241022/wanghanmo/chipyard-isax/.conda-env/share/verilator/include/verilated_funcs.h \
- /mnt/disk/newhome_20241022/wanghanmo/chipyard-isax/.conda-env/share/verilator/include/verilated_vcd_c.h \
- /mnt/disk/newhome_20241022/wanghanmo/chipyard-isax/.conda-env/share/verilator/include/verilated.h \
- /mnt/disk/newhome_20241022/wanghanmo/chipyard-isax/.conda-env/share/verilator/include/verilated_trace.h
